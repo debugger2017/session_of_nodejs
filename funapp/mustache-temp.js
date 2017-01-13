171164788c20bb0fc82ebed0a87d977b7b245081
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-13 18:15:50 +0530

    Added code of mustache demo

---
+++ funapp/mustache-temp.js
@@ -0,0 +1,10 @@
+var mustache = require('mustache')
+
+result = mustache.render("Hello {(first_name)} {(last_name)}",
+{
+	"first_name" : "Kartik"
+	"last_name" : "Satoskar"
+}
+)
+
+console.log(result)
