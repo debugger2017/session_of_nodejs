f8e5caa00c7d175784436ea8fdb19a651e604086
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-03 18:34:02 +0530

    Blocked ip

--- session2/app.js
+++ session2/app.js
@@ -4,15 +4,14 @@ var app = express();
 
 var publicPath = path.resolve(__dirname, 'public');
 
-app.use(express.static(publicPath));
-
-app.use(function(req,res,next){
-	console.log("Inside a middleware" + req.url);
-	next();
-})
-
-app.use(function(req,res){
-	res.status(200).send('Hello world')
+app.use(function(req, res,next){
+	if(req.ip = '::ffff:127.0.0.1')
+		res.status(404).send('Blocked')
+	else
+		next();
 });
 
-app.listen(3007);
+app.use(function(req, res){
+	res.status(200).send('OKKK')
+});
+app.listen(3008);
