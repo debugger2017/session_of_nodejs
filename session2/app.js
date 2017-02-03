2a8f50e363ed17dbfaadef304d880b33d7baf83a
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-03 17:54:32 +0530

    Added nodemon

--- session2/app.js
+++ session2/app.js
@@ -4,15 +4,15 @@ var app = express();
 
 var publicPath = path.resolve(__dirname, 'public');
 
-app.use(static(publicPath))
+app.use(static(publicPath));
 
 app.use(function(req,res,next){
 	console.log("Inside a middleware" + req.url);
 	next();
 })
 
-app.use(function(req,res)) {
+app.use(function(req,res){
 	req.status(200).send('File not found')
-}
+})
 
 app.listen(3007);
