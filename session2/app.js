55e93ee636fe273ceddcf87f2b0445ecc4d88501
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-03 17:49:49 +0530

    Did something

---
+++ session2/app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var path = require('path')
+var app = express();
+
+var publicPath = path.resolve(__dirname, 'public');
+
+app.use(static(publicPath))
+
+app.use(function(req,res,next){
+	console.log("Inside a middleware" + req.url);
+	next();
+})
+
+app.use(function(req,res)) {
+	req.status(200).send('File not found')
+}
+
+app.listen(3007);
