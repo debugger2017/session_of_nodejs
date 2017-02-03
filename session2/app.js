f169f86f243109e1313844d7e501491dc8ae718a
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-03 17:58:35 +0530

    Added app.js

--- session2/app.js
+++ session2/app.js
@@ -4,7 +4,7 @@ var app = express();
 
 var publicPath = path.resolve(__dirname, 'public');
 
-app.use(static(publicPath));
+app.use(express.static(publicPath));
 
 app.use(function(req,res,next){
 	console.log("Inside a middleware" + req.url);
@@ -12,7 +12,7 @@ app.use(function(req,res,next){
 })
 
 app.use(function(req,res){
-	req.status(200).send('File not found')
-})
+	res.status(200).send('Hello world')
+});
 
 app.listen(3007);
