e275dd7b7024ac7b7cfd8f735615357c52ff7e71
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-09 10:26:35 +0530

    Modified

--- session2/app.js
+++ session2/app.js
@@ -4,14 +4,7 @@ var app = express();
 
 var publicPath = path.resolve(__dirname, 'public');
 
-app.use(function(req, res,next){
-	if(req.ip = '::ffff:127.0.0.1')
-		res.status(404).send('Blocked')
-	else
-		next();
+app.get('/users/:username', function(req,res){
+	res.end("Hello ", req.params.username);
 });
-
-app.use(function(req, res){
-	res.status(200).send('OKKK')
-});
-app.listen(3008);
+app.use()
